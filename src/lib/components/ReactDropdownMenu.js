af22d7bc7bfb438d7c06a338e00024dfbf3bd2b4
MorganeLM <morganelemarquer@lilo.org>
2022-11-06 18:37:17 +0100

    add style

--- src/lib/components/ReactDropdownMenu.js
+++ src/lib/components/ReactDropdownMenu.js
@@ -8,7 +8,7 @@ function ReactDropdownMenu(props){
 
   return (
     <select id={props.id}
-            className="react-dropdown-menu"
+            className="react-dropdown-menu form-select"
             onChange={(e)=>handleChange(e)}>
       {props.options.map(option => (
         <option key={option.name} value={option.name}>{option.name}</option>
